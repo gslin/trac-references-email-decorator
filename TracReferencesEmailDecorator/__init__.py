104d7d2ccf68810703fea5549bc61b9be266359a
Gea-Suan Lin <gslin@gslin.org>
2019-09-29 14:58:08 +0800

    Move code to __init__py.

--- TracReferencesEmailDecorator/__init__.py
+++ TracReferencesEmailDecorator/__init__.py
@@ -1,1 +1,12 @@
-name = "TracReferencesEmailDecorator"
+# -*- coding: utf-8 -*-
+
+from trac.core import Component, implements
+from trac.notification.api import IEmailDecorator
+from trac.notification.mail import set_header
+
+class ReferencesEmailDecorator(Component):
+    implements(IEmailDecorator)
+
+    def decorate_message(self, event, message, charset):
+        if not 'References' in message:
+            set_header(message, 'References', message['Message-ID'], charset)
